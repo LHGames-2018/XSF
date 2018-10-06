318d20fcf8fe2c955da358e60f89bfd5b3f1cca4
Simon Berhanu <simon.berhanu@polymtl.ca>
2018-10-06 15:48:19 -0400

    2e essai

--- LHGames/Bot/Bot.cs
+++ LHGames/Bot/Bot.cs
@@ -7,8 +7,11 @@ namespace LHGames.Bot
     internal class Bot
     {
         internal IPlayer PlayerInfo { get; set; }
-        private int _currentDirection = 1;
-        private 
+
+        private int[] _currentDirection = new int[] {0,0};
+        private Boolean goingHome = false;
+
+
         internal Bot() { }
 
         /// <summary>
@@ -28,37 +31,93 @@ namespace LHGames.Bot
         /// <returns>The action you wish to execute.</returns>
         internal string ExecuteTurn(Map map, IEnumerable<IPlayer> visiblePlayers)
         {
-            // TODO: Implement your AI here.
-           
-            if (map.GetTileAt(PlayerInfo.Position.X + _currentDirection, PlayerInfo.Position.Y) == TileContent.Wall)
+            _currentDirection[0] = 0;
+            _currentDirection[1] = 0;
+            if (goingHome)
             {
-                _currentDirection *= -1;
 
-                for (int i = -10; i < 10; i++)
+                getDirectionTo(TileContent.House, map);
+            }
+            else
+            {
+                getDirectionTo(TileContent.Resource, map);
+
+            }
+            if (map.GetTileAt(PlayerInfo.Position.X, PlayerInfo.Position.Y) == TileContent.House && goingHome == true)
+            {
+               
+                goingHome = false;
+                return AIHelper.CreateMoveAction(new Point(0, 0));
+            }
+            if (map.GetTileAt(PlayerInfo.Position.X + _currentDirection[0], PlayerInfo.Position.Y + _currentDirection[1]) == TileContent.Resource)
+            {
+                Console.WriteLine(PlayerInfo.TotalResources);
+                if(PlayerInfo.CarriedResources == PlayerInfo.CarryingCapacity)
                 {
-                    for (int j = -10; i < 10; j++)
+                goingHome = true;
+
+                }
+                getDirectionTo(TileContent.Resource, map);
+                return AIHelper.CreateCollectAction(new Point(_currentDirection[0], _currentDirection[1]));
+            }
+            return AIHelper.CreateMoveAction(new Point(_currentDirection[0], _currentDirection[1]));
+
+
+
+        }
+        internal void getDirectionTo(TileContent tile,Map map)
+        {
+            int lowest = 20;
+            int Xdistance = 0;
+            int Ydistance = 0;
+            for (int dx = -9; dx <= 9; dx++)
+            {
+                for (int dy = -9; dy <= 9; dy++)
+                {
+                    if (map.GetTileAt(PlayerInfo.Position.X + dx, PlayerInfo.Position.Y + dy) == tile)
                     {
-                        if (map.GetTileAt(PlayerInfo.Position.X + i, PlayerInfo.Position.Y + j) == TileContent.Resource)
+                        int total = Math.Abs(dx) + Math.Abs(dy);
+                        if (total <= lowest)
                         {
-                            while (PlayerInfo.Position.X != PlayerInfo.Position.X + i)
-                            {
-
-                                _currentDirection = 0;
-                            }
+                            Xdistance = dx;
+                            Ydistance = dy;
+                            lowest = total;
                         }
-
+                        Console.WriteLine("total: " + total);
                     }
                 }
             }
 
+            if (lowest != 0)
+            {
+                if (Math.Abs(Xdistance) >= Math.Abs(Ydistance) || map.GetTileAt(PlayerInfo.Position.X, PlayerInfo.Position.Y + (Ydistance/Ydistance)) == TileContent.Resource)
+                {
+                    if (tile != TileContent.Resource || Xdistance != 0)
+                    {
 
-            
+                        _currentDirection[0] = Xdistance / (Math.Abs(Xdistance));
+                        _currentDirection[1] = 0;
+                    }
+                    else {
+                        if (Math.Abs(Ydistance) != 0)
+                        {
+                        _currentDirection[1] = Ydistance / (Math.Abs(Ydistance));
 
-            var data = StorageHelper.Read<TestClass>("Test");
-            Console.WriteLine(data?.Test);
-            return AIHelper.CreateMoveAction(new Point(_currentDirection, 0));
+                        _currentDirection[0] = 0;
+                        }
+
+                    }
+
+                    Console.WriteLine("x");
+                }
+                else
+                {
+                    _currentDirection[1] = Ydistance / (Math.Abs(Ydistance));
+                    _currentDirection[0] = 0;
+                    Console.WriteLine("y");
+                }
+            }
         }
-
         /// <summary>
         /// Gets called after ExecuteTurn.
         /// </summary>
