8587e91ed8554b5ebb223e56e91335dfc016b6ff
Facundo Giannotti <facundogiannotti@gmail.com>
2018-10-06 16:01:44 -0400

    changed pathfinding

--- LHGames/Bot/Bot.cs
+++ LHGames/Bot/Bot.cs
@@ -161,8 +161,32 @@ namespace LHGames.Bot
                 }
                 else
                 {
-                    _currentDirection[1] = Ydistance / (Math.Abs(Ydistance));
-                    _currentDirection[0] = 0;
+                    if (map.GetTileAt(PlayerInfo.Position.X, PlayerInfo.Position.Y + (Ydistance / Ydistance)) == TileContent.Resource)
+                    {
+                        if (Math.Abs(Xdistance) != 0)
+                        {
+                            _currentDirection[0] = Xdistance / (Math.Abs(Xdistance));
+
+                            _currentDirection[1] = 0;
+                        }
+                        else if ((Math.Abs(Xdistance) + Math.Abs(Ydistance)) != 1)
+                        {
+                            _currentDirection[0] = 1;
+                            _currentDirection[1] = 0;
+                        }
+                        else
+                        {
+                            _currentDirection[1] = Ydistance / (Math.Abs(Ydistance));
+
+                            _currentDirection[0] = 0;
+                        }
+                    }
+                    else
+                    {
+                        _currentDirection[1] = Ydistance / (Math.Abs(Ydistance));
+
+                        _currentDirection[0] = 0;
+                    }
                     Console.WriteLine("y");
                 }
             }
