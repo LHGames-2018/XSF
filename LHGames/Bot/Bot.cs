68223fd4e0942c7906f6614768e7a0d8a2f74fa9
Bruno  Curzi-Laliberte <bruno.curzi-laliberte@polymtl.ca>
2018-10-06 13:21:13 -0400

    added for loops

--- LHGames/Bot/Bot.cs
+++ LHGames/Bot/Bot.cs
@@ -7,7 +7,7 @@ namespace LHGames.Bot
     internal class Bot
     {
         internal IPlayer PlayerInfo { get; set; }
-        private int _currentDirection = 1;
+        private int[] _currentDirection = new int[] {0,0};
 
         internal Bot() { }
 
@@ -28,15 +28,50 @@ namespace LHGames.Bot
         /// <returns>The action you wish to execute.</returns>
         internal string ExecuteTurn(Map map, IEnumerable<IPlayer> visiblePlayers)
         {
+            int lowest = 0;
+            int Xdistance = 0;
+            int Ydistance = 0;
+            for (int dx = -9; dx <= 9; dx++)
+            {
+                for (int dy = -9; dy <= 9; dy++)
+                    {
+                        if (map.GetTileAt(PlayerInfo.Position.X + dx, PlayerInfo.Position.Y  + dy) == TileContent.Resource){
+                            int total = Math.Abs(dx)+ Math.Abs(dy);
+                            if (total <= lowest){
+                                Xdistance = dx;
+                                Ydistance = dy;
+                                lowest = total;
+                            }
+                            Console.WriteLine(total);
+                        }
+                    }
+            }
+
+            if(lowest != 0){
+                if (Math.Abs(Xdistance)>=Math.Abs(Ydistance)){
+                    _currentDirection[0] = Xdistance/(Math.Abs(Xdistance));
+                    _currentDirection[1] = 0;
+                }
+                else
+                {
+                    _currentDirection[1] = Ydistance/(Math.Abs(Ydistance));
+                    _currentDirection[0] = 0;
+                }
+            }
+
+            return AIHelper.CreateMoveAction(new Point(_currentDirection[0], _currentDirection[1]));
+            
             // TODO: Implement your AI here.
-            if (map.GetTileAt(PlayerInfo.Position.X + _currentDirection, PlayerInfo.Position.Y) == TileContent.Wall)
+            /*
+            if (map.GetTileAt(PlayerInfo.Position.X, PlayerInfo.Position.Y  + _currentDirection) == TileContent.Wall)
             {
                 _currentDirection *= -1;
             }
+            */
 
             var data = StorageHelper.Read<TestClass>("Test");
-            Console.WriteLine(data?.Test);
-            return AIHelper.CreateMoveAction(new Point(_currentDirection, 0));
+            //Console.WriteLine(data?.Test);
+            return AIHelper.CreateMoveAction(new Point(0, 0));
         }
 
         /// <summary>
